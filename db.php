6f4f06caa96247ca84ec2485e5fe45ea0230ae89
Sanjana <asanjana0823@gmail.com>
2025-02-06 14:17:48 +0530

    Add index file

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = 'yourMySQLRandomPwd';
+$dbpass = '';
 $dbname = 'homteq';
 //create a DB connection
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //if the DB connection fails, display an error message and exit
-if (!$conn)
-{
- die('Could not connect: ' . mysqli_error($conn));
+if (!$conn) {
+    die('Could not connect: ' . mysqli_error($conn));
 }
 //select the database
 mysqli_select_db($conn, $dbname);
-?>
