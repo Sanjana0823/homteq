d2035eb3acc4ee3187b0ba4f364d2036a4c744c7
Sanjana <asanjana0823@gmail.com>
2025-03-10 23:54:18 +0530

    update product file

--- prodbuy.php
+++ prodbuy.php
@@ -14,7 +14,10 @@ $prodid=$_GET['u_prod_id'];
 //display the value of the product id, for debugging purposes
 echo "<p>Selected product Id: ".$prodid;
 
-$SQL="select prodId, prodName, prodPicNameLarge, prodDescripLong, prodPrice, prodQuantity from Product";
+//create a $SQL variable and populate it with a SQL statement that retrieves details for the selected product
+$SQL="select prodId, prodName, prodPicNameSmall, prodDescripLong, prodPrice, prodQuantity
+from Product
+where prodId=".$prodid;
 //run SQL query for connected DB or exit and display error message
 $exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
 echo "<table style='border: 0px'>";
@@ -23,23 +26,36 @@ echo "<table style='border: 0px'>";
 //Iterate through the array i.e while the end of the array has not been reached, run through it
 while ($arrayp=mysqli_fetch_array($exeSQL))
 {
-echo "<tr>";
-echo "<td style='border: 0px'>";
-//make the image into an anchor to prodbuy.php and pass the product id by URL (the id from the array)
-echo "<a href=prodbuy.php?u_prod_id=".$arrayp['prodId'].">";
+    echo "<table style='border: 0px'>";
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
+    echo "<img src=/images/".$arrayp['prodPicNameSmall']." height=350 width=350>";
+    echo "</td>";
+    echo "<td style='border: 0px'>";
+    echo "<p><h5>".$arrayp['prodName']."</h5></p>";
+    echo "<br><p>".$arrayp['prodDescripLong']."</p>";
+    echo "<br><p><b>&pound".$arrayp['prodPrice']."</b></p>";
+    echo "<br><p>Number left in stock: ".$arrayp['prodQuantity'] ."</p>";
+    echo "</td>";
+    echo "</tr>";
+    echo "</table>";
 
-//display the small image whose name is contained in the array
-echo "<img src=images/".$arrayp['prodPicNameSmall']." height=200 width=200>";
-echo "</a>";
-echo "</td>";
-echo "<td style='border: 0px'>";
-echo "<p><h5>".$arrayp['prodName']."</h5>"; //display product name as contained in the array
-echo "<p>".$arrayp['prodDescripShort']."</p>"; //display product description as contained in the array
-echo "<p></br><b>Rs. ".$arrayp['prodPrice']."</b></p>"; //display product description as contained in the array
-echo "</td>";
-echo "</tr>";
+    echo "<br><p>Number to be purchased: ";
+    //create a form made of one drop-down menu and one button for user to enter quantity
+    //the value entered in the form will be posted to the basket.php to be processed
+    echo "<form action=basket.php method=post>";
+    echo "<select name=p_quantity>";
+    for ($i=1; $i<=$arrayp['prodQuantity']; $i++)
+    {
+    echo "<option value=".$i.">".$i."</option>";
+    }
+    echo "</select>";
+    echo "<input type=submit name='submitbtn' value='ADD TO BASKET' id='submitbtn'>";
+    //pass the product id to the next page basket.php as a hidden value
+    echo "<input type=hidden name=h_prodid value=".$prodid.">";
+    echo "</form>";
+    echo "</p>";
 }
-echo "</table>";
 
 include("footfile.html"); //include head layout
 echo "</body>";
